909905620768f0aa56d9a1efe6151e46c8c60a30
ayush369 <ayushdave369@gmail.com>
2024-08-01 16:02:22 +0530

    commit

---
+++ components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark p-3">
+        <div class="container-fluid">
+          <NuxtLink class="navbar-brand ms-3" to="/">Logo</NuxtLink>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item me-3">
+                   
+                    <NuxtLink class="nav-link" to="/cart"> <i class="fas fa-thin fa-cart-shopping me-2"></i>Cart</NuxtLink>
+                </li>
+                <li class="nav-item me-3">
+                  <NuxtLink class="nav-link" to="/login"><i class="fas fa-light fa-right-to-bracket me-2"></i>Login</NuxtLink>
+                </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Navbar'
+  }
+  </script>
+  
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <Navbar/>
+        <Hello/>
+    </div>
+</template>
